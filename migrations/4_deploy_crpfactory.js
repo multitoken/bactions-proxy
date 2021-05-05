05da0fbc76271b944b2b32b8ad8c7bdf1fb861f3
rremizov <rremizov@yandex.ru>
2021-05-05 16:48:06 +0500

    Update

--- migrations/4_deploy_crpfactory.js
+++ migrations/4_deploy_crpfactory.js
@@ -6,10 +6,9 @@ const BActions = artifacts.require('BActions');
 const BFactory = artifacts.require('BFactory');
 
 module.exports = async function(deployer, network, accounts) {
-    if (network == 'development' || network == 'soliditycoverage') {
+    if (network == 'kovan-fork' || network == 'kovan') {
         await deployer.deploy(RightsManager);
         await deployer.deploy(SmartPoolManager);
-        await deployer.deploy(BFactory);
         await deployer.deploy(BalancerSafeMath);
 
         await deployer.link(BalancerSafeMath, CRPFactory);
@@ -17,9 +16,5 @@ module.exports = async function(deployer, network, accounts) {
         await deployer.link(SmartPoolManager, CRPFactory);
 
         await deployer.deploy(CRPFactory);
-
-        await deployer.deploy(BActions, BFactory.address);
-    } else if (network == 'kovan-fork' || network == 'kovan') {
-        deployer.deploy(BActions, '0x418a1cEe2715594b8730a5adFeA75ecAF02b7b31');
     }
 }
